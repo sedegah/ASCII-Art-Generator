5ab0f1c0bf16a2b65b286a5d788a92a37220b24d
Kimathi Sedegah <sedkim370@gmail.com>
2025-05-25 14:10:24 +0100

    Create main.dart

---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('ASCII Art Generator');
+  print('Usage:');
+  print('  dart run bin/ascii_text.dart "Hello"');
+  print('  dart run bin/ascii_image.dart --input path_to_image --width 80');
+}
