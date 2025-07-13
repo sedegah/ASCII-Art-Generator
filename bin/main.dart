00533358e6f2c94f1211823ef0ca63adf5bc6e88
Kimathi Sedegah <sedkim370@gmail.com>
2025-07-13 09:27:49 +0000

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,9 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  print('ASCII Art Generator');
-  print('Usage:');
-  print('  dart run bin/ascii_text.dart "Hello"');
-  print('  dart run bin/ascii_image.dart --input path_to_image --width 80');
+void main(List<String> args) {
+  print('ASCII Art Generator (Dart version)');
+  if (args.isEmpty) {
+    print('Usage: dart run bin/main.dart <image_path>');
+    exit(1);
+  }
 }
